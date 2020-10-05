c29c1a9621bfceff7a305f246ea1d8cc4c99c73b
kimura-12 <kimura@ai.cs.kobe-u.ac.jp>
2020-10-06 03:31:46 +0900

    ABC170

--- AtCoder_Beginner_Contest/ABC170/D.Not_Divisible/main.py
+++ AtCoder_Beginner_Contest/ABC170/D.Not_Divisible/main.py
@@ -1,11 +1,16 @@
 n = int(input())
 a = list(map(int, input().split()))
+dp = [0] * (max(a) + 1)
+a.sort()
 
-count = 0
-for i in range(n):
-    for j in range(i, n):
-        if i == j:
-            continue
-        if a[i] % a[j] != 0:
-            count += 1
-print(count)
+for i in a:
+  temp = i
+  while temp <= a[-1]:
+    dp[temp] += 1
+    temp += i
+ans = 0
+for i in a:
+  if dp[i] == 1:
+    ans += 1
+             
+print(ans)
